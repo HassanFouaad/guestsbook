743577ece4052e94ecf31acf73f6a2546e284206
HassanFouaad <mrhassanfouad1@gmail.com>
2020-09-16 14:38:35 +0200

    Auth Reducer Cases

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -18,12 +18,40 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    default:
     case USER_LOADING:
       return {
         ...state,
         isLoading: true,
       };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+    case LOGIN_FAIL:
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT_SUCCESS:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    default:
       return state;
   }
 }
