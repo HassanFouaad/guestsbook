a323ecb4fa6cb6472c6183a4d54003a90c43bedd
HassanFouaad <mrhassanfouad1@gmail.com>
2020-09-16 13:13:26 +0200

    Message Schema

---
+++ models/Message.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const Schema = mongoose.Schema;
+const messageSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    subject: {
+      type: String,
+      required: true,
+      minlength: 1,
+    },
+    text: {
+      type: String,
+      required: true,
+      minlength: 1,
+    },
+    replies: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+messageSchema.plugin(uniqueValidator);
+module.exports = Message = mongoose.model("Message", messageSchema);
