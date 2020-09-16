cea1a8c410f2297f3e88c2ba511bb826287bd5f0
HassanFouaad <mrhassanfouad1@gmail.com>
2020-09-16 14:24:17 +0200

    Creating Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import ReduxToastr from "react-redux-toastr";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Provider store={store}>
+      <ReduxToastr
+        timeOut={4000}
+        newestOnTop={false}
+        preventDuplicates
+        position="top-left"
+        getState={(state) => state.toastr}
+        transitionIn="fadeIn"
+        transitionOut="fadeOut"
+        progressBar
+        closeOnToastrClick
+      ></ReduxToastr>
+    </Provider>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Fragment>
     <App />
-  </React.StrictMode>,
+  </Fragment>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
