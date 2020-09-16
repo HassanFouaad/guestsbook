31812954c84641d6b72f75f34e0815b7aa844973
HassanFouaad <mrhassanfouad1@gmail.com>
2020-09-16 13:20:58 +0200

    Signup inputs validator

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { signUp } = require("../controllers/user");
-
+const { userSignUpValidator, validate } = require("../validator/user");
 //Routes
-router.post("/signup", signUp);
+router.post("/signup", userSignUpValidator(), validate, signUp);
 //Export
 module.exports = router;
---
+++ validator/user.js
@@ -0,0 +1,36 @@
+const { validationResult, check } = require("express-validator");
+
+const userSignUpValidator = () => {
+  return [
+    check("firstname", "First name is required!").notEmpty(),
+    check("lastname", "Last name is required!").notEmpty(),
+    check("email", "Email is required")
+      .notEmpty()
+      .withMessage("Please Enter your Email")
+      .isEmail()
+      .withMessage("Please Enter a valid Email Address"),
+    check("password", "Password is required").notEmpty(),
+
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must contain at least 6 characters"),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+    // Build your resulting errors however you want! String, object, whatever - it works!
+    return `${msg}`;
+  };
+  const result = validationResult(req).formatWith(errorFormatter);
+  if (!result.isEmpty()) {
+    // Response will contain something like
+    // { errors: [ "body[password]: must be at least 10 chars long" ] }
+    return res.status(400).json({ error: result.array()[0] });
+  }
+  return next();
+};
+module.exports = {
+  userSignUpValidator,
+  validate,
+};
